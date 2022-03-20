e10b94137d72a2a5ce9cf277455f12cea298300f
Princewill Chiaka <9r1nc3w1ll@gmail.com>
2022-03-20 21:51:34 +0000

    chore: Add live db connection

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,24 @@
 // Update with your config settings.
 
+require('dotenv').config();
+
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-  client: 'sqlite3',
+  client: 'postgres',
   connection: {
-    filename: './dev.sqlite3'
+    database: 'd64svpq1i8rhq6',
+    user:     'myzzevbdqmhwgs',
+    password: 'f9e07d1a0baf090fd2916796b5fa1fd78d9a6bae8d702a15d7d55a911d9852aa',
+    host: 'ec2-54-76-249-45.eu-west-1.compute.amazonaws.com',
+    ssl: { rejectUnauthorized: false },
   },
-  useNullAsDefault: true,
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  },
 };
